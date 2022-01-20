c8926f9b16c1b9c5af4e17629b68d794f8d47e7d
Denis Paul <denis.paul@softvision.com>
2022-01-20 19:28:42 +0200

    added class method for getting city weather data

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,5 +1,12 @@
 class Destination < ApplicationRecord
     validates :city, presence: true, uniqueness: true
     validates :country, presence: true
-    validates :currency, presence: true, length: { maximum: 3, message: "doesn't have a valid size" } 
+    validates :currency, presence: true, length: { maximum: 3, message: "doesn't have a valid size" }
+    
+    def self.get_weather(city)
+        @response =  RestClient.get "http://api.weatherapi.com/v1/current.json?q=#{city}", { content_type: :json, accept: :json, "key": ENV["WEATHER_API_KEY"] }
+        # byebug
+        @current_city_weather = JSON.parse(@response.body)["current"]
+        @current_city_weather
+    end
 end
