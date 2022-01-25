1c789d800336d3560227606da4dd2b7bf94e239b
Denis Paul <denis.paul@softvision.com>
2022-01-25 18:44:44 +0200

    changed tests for destination model

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe Destination, type: :model do
 
     describe 'validations' do
         context 'city' do
-            it { should validate_presence_of(:city) }
-            it { should validate_uniqueness_of(:city) }
+            it { is_expected.to validate_presence_of(:city) }
+            it { is_expected.to validate_uniqueness_of(:city) }
         end
 
         context 'country' do
-            it { should validate_presence_of(:country) }
+            it { is_expected.to validate_presence_of(:country) }
         end
 
         context 'currency' do
-            it { should validate_presence_of(:currency) }
-            it { should validate_length_of(:currency).is_at_most(3).with_message("doesn't have a valid size") }
+            it { is_expected.to validate_presence_of(:currency) }
+            it { is_expected.to validate_length_of(:currency).is_at_most(3).with_message("doesn't have a valid size") }
         end
     end
 end
