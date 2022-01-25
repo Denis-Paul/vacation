a8d7223fdfc2cc32bc210a579318226703be78a4
Denis Paul <denis.paul@softvision.com>
2022-01-25 16:23:14 +0200

    changed exception handling

--- app/services/weather_api_service.rb
+++ app/services/weather_api_service.rb
@@ -1,5 +1,6 @@
 class WeatherApiService < ApplicationService
     attr_reader :city
+    attr_accessor :errors
 
     def initialize(city)
       @city = city
@@ -14,12 +15,20 @@ class WeatherApiService < ApplicationService
     def get_weather
       begin
         response =  RestClient.get "http://api.weatherapi.com/v1/current.json?q=#{@city}", { content_type: :json, accept: :json, "key": ENV["WEATHER_API_KEY"] }
+        # byebug
+        # response.code
         current_weather = JSON.parse(response.body, symbolize_names: true)[:current]
         current_weather
+      # rescue RestClient::ExceptionWithResponse => exception
+      #   byebug
+      #   exception.response
       rescue => exception
         # exception.class.name
         # exception.message
+        # byebug
         api_error_message = JSON.parse(exception.response, symbolize_names: true)[:error][:message]
+        self.errors = api_error_message
+        self
       end
     end
 end
