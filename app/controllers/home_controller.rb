305d64140c7a521f2c4aefc9cb1f884af9e5ef79
Denis Paul <denis.paul@softvision.com>
2022-01-31 15:05:14 +0200

    added city weather data to destinations table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @destinations = Destination.all
+    @destinations_weather_data = {}
+    @destinations.each do |destination|
+      city_weather = WeatherApiService.call(destination.city)
+      @destinations_weather_data["#{destination.city}"] = city_weather
+    end
   end
 end
