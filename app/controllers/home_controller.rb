23620521f36c2748445477ed4b232f811492b9e2
Denis Paul <denis.paul@softvision.com>
2022-02-02 16:11:21 +0200

    added a comment with potentially useful command

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @destinations = Destination.all
+    # @destinations = Destination.joins(:reviews).group("destinations.id").order("avg(reviews.rating) desc")
   end
 end
