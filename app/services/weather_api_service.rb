1de99aa6b25fda22977025dd1361affbc8b3f45d
Denis Paul <denis.paul@softvision.com>
2022-02-03 11:57:50 +0200

    refactoring

--- app/services/weather_api_service.rb
+++ app/services/weather_api_service.rb
@@ -15,8 +15,8 @@ class WeatherApiService < ApplicationService
     def get_weather
       begin
         response =  RestClient.get "http://api.weatherapi.com/v1/current.json?q=#{@city}", { content_type: :json, accept: :json, "key": ENV["WEATHER_API_KEY"] }
-        current_weather = JSON.parse(response.body, symbolize_names: true)[:current]
-        current_weather
+        current_weather = JSON.parse(response.body, symbolize_names: true)
+        current_weather[:current]
       rescue RestClient::ExceptionWithResponse => exception
         api_error_message = JSON.parse(exception.response, symbolize_names: true)[:error][:message]
         api_error_message
